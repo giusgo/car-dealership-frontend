69a576c0f814917ec5f20e84a99d38e4407ec33f
giusgo <giuseppeg@uninorte.edu.co>
2023-11-26 16:22:19 -0500

    Catalogue done

--- src/js/catalogue.js
+++ src/js/catalogue.js
@@ -1,13 +1,63 @@
 import {addLogoutButton, setHeaderButtons} from "./loginController";
-import {addRedirectionToButtons} from "./redirect";
+import {addRedirectionToButtons, redirectTo} from "./redirect";
 import {getCatalogue} from "./requests";
+import * as car_images from "../assets/car-imgs/*.png";
+import {cookieExists} from "./cookie";
+
+function createItem(imageName, name, price) {
+
+    let itemObject = `
+        <div class="item">
+
+        <div class="img">
+        <img src="${car_images[imageName.replace(".png", "")]}" alt="">
+        </div>
+        <div class="name">
+        <p>${name}</p>
+        </div>
+        <div class="price">
+        <p>$${price}</p>
+        </div>
+
+        </div>
+        `;
+
+    return itemObject;
+
+}
+
+function customizeCar(car_model) {
+    
+    // Login if not logged in
+    if (!cookieExists()) {
+        window.location.href = `/login?car_model=${car_model}`;
+        return;
+    }
+
+    // If logged in
+    window.location.href = `/customize?car_model=${car_model}`;
+
+}
 
 async function generateCatalogue() {
 
-    var response = await getCatalogue();
-    console.log(response);
+    const catalogueList = $('#catalogue-list');
 
+    var catalogue = await getCatalogue();
+    console.log(catalogue);
 
+    for (let i = 0; i <= 4; i++) {
+
+        for (let object of catalogue) {
+            let item = createItem(object.carImage, object.brandName, object.priceBase);
+            catalogueList.append(item);
+
+            // Add redirect function
+            $('.item').last().click(function() {
+                customizeCar(object.model);
+            });
+        }
+    }
 
 }
 
